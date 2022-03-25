279f59bc96a09040736acbd0afe208400eb55e52
Your Name <you@example.com>
2022-03-25 09:40:03 +0400

    add upload functionality for gcp storage

--- airflow/dags/gcp.py
+++ airflow/dags/gcp.py
@@ -1,7 +1,9 @@
 # Imports the Google Cloud client library
 from google.cloud import storage
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/vaso/PycharmProjects/yellow_taxi_gcp/seismic-hope-345015-78d3a80b6173.json"
+
+os.environ[
+    "GOOGLE_APPLICATION_CREDENTIALS"] = "/home/vaso/PycharmProjects/yellow_taxi_gcp/seismic-hope-345015-78d3a80b6173.json"
 
 # Instantiates a client
 storage_client = storage.Client()
@@ -10,6 +12,20 @@ storage_client = storage.Client()
 bucket_name = "my-new-bucket_jj"
 
 # Creates the new bucket
-bucket = storage_client.create_bucket(bucket_name)
+# bucket = storage_client.create_bucket(bucket_name)
 
-print("Bucket {} created.".format(bucket.name))
+# print("Bucket {} created.".format(bucket.name))
+
+source_file_name = '/home/vaso/PycharmProjects/yellow_taxi_gcp/partitions/yellow_taxi_2015_01_04.csv'
+destination_blob_name = 'yellow_taxi_2015_01_04.csv'
+
+bucket = storage_client.bucket(bucket_name)
+
+blob = bucket.blob(destination_blob_name)
+
+blob.upload_from_filename(source_file_name)
+
+print(
+    "File {} uploaded to {}.".format(
+        source_file_name, destination_blob_name)
+)
